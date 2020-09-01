31896cc88bc5938f693f46935bca01d287c299ef
SonyUSA <sirius.cybernetics@gmail.com>
2020-09-01 02:08:01 -0600

    Use the right names!

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -585,8 +585,8 @@ static void ui_draw_vision_map(UIState *s) {
 // This looks like a good spot for waifu code!
 static void ui_draw_waifu1(UIState *s) {
   const int waifu1_size = 100;
-  const int waifu1_x = (s->scene.ui_viz_rx + (waifu_size * 3) + (bdr_is * 3));
-  const int waifu1_y = (footer_y + ((footer_h - waifu_size) / 2));
+  const int waifu1_x = (s->scene.ui_viz_rx + (waifu1_size * 3) + (bdr_is * 3));
+  const int waifu1_y = (footer_y + ((footer_h - waifu1_size) / 2));
   ui_draw_circle_image(s->vg, waifu1_x, waifu1_y+border_shifter+25, waifu1_size-5, s->img_waifu1, s->scene.waifu1_valid);
 }
 // End of waifus
