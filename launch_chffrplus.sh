f5cc2f494077da75dcdafbbf6af644cae6318bf5
SonyUSA <sirius.cybernetics@gmail.com>
2020-10-23 05:04:19 -0600

    I'm bad at *nix

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -75,9 +75,9 @@ function launch {
   chmod 777 /data/openpilot/apk/chrome.apk
   pm install -r -d /data/openpilot/apk/chrome.apk
   # lets move a local copy of autoecu.io to the SD <1MB -SonyUSA
-  mv /data/openpilot/autoecu.io/*.* /storage/emulated/0/
+  mv /data/openpilot/autoecu.io/* /storage/emulated/0/
   # If we need to, we can go online with the original line uncommented -SonyUSA
-  am start -n com.android.chrome/com.google.android.apps.chrome.Main -d /storage/emulated/0/index.html
+  am start -n com.android.chrome/com.google.android.apps.chrome.Main -d /sdcard/index.html
   # am start -n com.android.chrome/com.google.android.apps.chrome.Main -d autoecu.io
   mv /data/openpilot/rwds/*.rwd /storage/emulated/0/
 
